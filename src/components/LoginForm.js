308ce81d51aefe66f8468a60b1cd735e5b3491a5
Robert <lactoz22@gmail.com>
2022-10-18 14:45:42 -0500

    Removed the name field. Created a conditional rendering of the button to show if the log in / sign up failed. Imported a helper function to post to server the newly created user. Imported a helper function to check if the user trying to log in does exist

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,40 +1,82 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { loginUser } from '../Services/login.js';
+import { createUser } from '../Services/users.js';
+import { setToken } from '../Services/feedbacks';
 
 function LoginForm({ setUser, title, signUp }) {
-  const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [success, setSuccess] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('Invalid input');
 
-  const submitHandler = () => {
-    const user = {
-      name,
-      username,
-      password
-    };
+  const submitHandler = async () => {
+    if (success) {
+      const user = {
+        username,
+        password
+      };
 
-    setUser(user);
+      if (signUp) {
+        try {
+          await createUser(user);
+        } catch (err) {
+          const res = err.response;
+          setErrorMessage(res.data.error);
+          setSuccess(false);
+        }
+      } else {
+        try {
+          const res = await loginUser({ username: user.username, password: user.password });
+          setUser(res);
+          window.localStorage.setItem('userFeedbackBlogCredentials', JSON.stringify(res));
+          setToken(res.token);
+        } catch (err) {
+          setSuccess(false);
+        }
+      }
+    }
+  };
+
+  const usernameHandler = ({ target }) => {
+    setUsername(target.value);
+    setSuccess(true);
+    setErrorMessage('');
+  };
+
+  const passwordHandler = ({ target }) => {
+    setPassword(target.value);
+    setSuccess(true)
+    setErrorMessage('');
   };
 
   const usernameClasses = (username.length !== 0 && username.length < 3) ? 'card invalid' : 'card';
   const passwordClasses = (password.length !== 0 && password.length < 4) ? 'card invalid' : 'card';
+  const buttonClasses = (success) ? '' : 'invalidButton';
+
+  const signInButtonText = (<>
+    {
+      success
+        ? 'Sign in'
+        : 'Invalid username or password'
+    }
+  </>);
+
+  const signUpButtonText = (<>
+    {
+      success
+        ? 'Sign up'
+        : errorMessage
+    }
+  </>);
 
   return (
     <div id="loginScreen" className="card">
       <h1>{title}</h1>
       <div id="loginForm">
         <form>
-          {
-            signUp
-            && <label htmlFor="FirstName">First name*:</label>
-          }
-          {
-            signUp
-            && <input type="text" className="card" onInput={(e) => setName(e.target.value)} />
-          }
-
           <label htmlFor="username">Username{signUp ? '*' : ''}:</label>
-          <input type="text" className={usernameClasses} onInput={(e) => setUsername(e.target.value)} />
+          <input type="text" className={usernameClasses} onChange={usernameHandler} value={username} />
 
           {
             signUp
@@ -42,7 +84,7 @@ function LoginForm({ setUser, title, signUp }) {
           }
 
           <label htmlFor="password">Password{signUp ? '*' : ''}:</label>
-          <input type="password" className={passwordClasses} onInput={(e) => setPassword(e.target.value)} />
+          <input type="password" className={passwordClasses} onChange={passwordHandler} value={password} />
 
           {
             signUp
@@ -50,10 +92,17 @@ function LoginForm({ setUser, title, signUp }) {
           }
 
           <button type="button"
-                  disabled={!name || !username || username.length < 3 || !password || password.length < 4}>Sign in
+                  disabled={success && (!username || username.length < 3 || !password || password.length < 4)}
+                  onClick={submitHandler}
+                  className={buttonClasses}
+          >
+            {
+              signUp
+                ? signUpButtonText
+                : signInButtonText
+            }
           </button>
         </form>
-
         {
           signUp
             ? <div>Already have an account? <Link to="/login">Sign in</Link></div>
